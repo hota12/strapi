fe9760581f61d1a6fffd790bcc638fde57583969
Anton Archer <anton_archer@hotmail.com>
2023-10-22 18:03:41 -0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -5,13 +5,13 @@ module.exports = ({ env }) => ({
       },
     },
     upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+       config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
         },
+      },
+    },
         actionOptions: {
           upload: {},
           delete: {},
